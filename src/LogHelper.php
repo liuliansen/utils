a1bcb259253566ad5f23490181560edc070a89c5
liansen <liansen@d3zz.com>
2018-07-06 15:51:19 +0800

    重写LogHelper

--- src/LogHelper.php
+++ src/LogHelper.php
@@ -8,77 +8,98 @@
 //+-------------------------------------------------------------
 namespace utils;
 
-use utils\log\driver\File;
-
 
 /**
  * Class LogHelper
  * @package utils
- * @method void log($msg,    $dir = '', $format = '') static 记录一般日志
- * @method void error($msg,  $dir = '') static 记录错误日志
- * @method void info($msg,   $dir = '') static 记录一般信息日志
- * @method void notice($msg, $dir = '') static 记录提示日志
- * @method void alert($msg,  $dir = '') static 记录报警日志
- * @method void debug($msg,  $dir = '') static 记录调试日志
+ * @method void log($msg,    $format = '') static 记录一般日志
+ * @method void error($msg,  $format = '') static 记录错误日志
+ * @method void info($msg,   $format = '') static 记录一般信息日志
+ * @method void notice($msg, $format = '') static 记录提示日志
+ * @method void alert($msg,  $format = '') static 记录报警日志
+ * @method void debug($msg,  $format = '') static 记录调试日志
  */
 class LogHelper
 {
-
-    static protected $fileSize = 2097152;
+    protected $config = [
+        'file_size'   => 2097152,
+        'path'        => '/var/log/',
+    ];
 
     /**
      * @var array 日志类型
      */
-    static  protected  $type = ['log', 'error', 'info', 'notice', 'alert', 'debug'];
+    protected static $type = ['log', 'error', 'info', 'sql', 'notice', 'alert', 'debug'];
+
 
     /**
-     * 写入日志
-     * @throws \Exception
+     * 尚未输出的日志信息
+     * @var array
      */
-    static public function record()
+    protected $logs = [];
+
+    /**
+     * LogHelper constructor.
+     * @param array $config
+     */
+    public function __construct($config = [])
     {
-        $argv = func_get_args();
-        $argc = count($argv);
-        if($argc == 2){
-            Log::record($argv[0],$argv[1]);
-            Log::save();
-            return;
-        }elseif($argc == 3){
-            $writer = new File(['path' => $argv[1]]);
-            $log = [$argv[2] => [$argv[0]]];
-            $writer->save($log);
-            return;
-        }elseif($argc == 4){
-            static::save($argv);
-            return;
+        if (is_array($config)) {
+            $this->config = array_merge($this->config, $config);
         }
-        throw new \Exception('不支持的日志参数');
     }
 
 
-    static protected function save($args)
+    /**
+     * 记录日志
+     * @param $type
+     * @param $log
+     * @param $format
+     * @return bool
+     */
+    protected function record($type,$log,$format = '')
     {
-        $destination = $args[1].date('Ym').DIRECTORY_SEPARATOR.date('d').'.log';
+        if(!in_array($type,static::$type)) {
+            return true;
+        }
+        return  $this->save($type,$log,$format);
+    }
+
+
+    /**
+     * 自定义格式存储
+     * @param $type
+     * @param $log
+     * @param string $format
+     * @return bool
+     */
+    protected function save($type,$log,$format = '')
+    {
+        $destination = $this->config['path'].DIRECTORY_SEPARATOR.date('Ym').DIRECTORY_SEPARATOR.date('d').'.log';
         $path = dirname($destination);
         !is_dir($path) && mkdir($path, 0755, true);
         //检测日志文件大小，超过配置大小则备份日志文件重新生成
-        if (is_file($destination) && floor(static::$fileSize) <= filesize($destination)) {
+        if (is_file($destination) && floor($this->config['file_size']) <= filesize($destination)) {
             try {
                 rename($destination, dirname($destination) . DIRECTORY_SEPARATOR . time() . '-' . basename($destination));
             } catch (\Exception $e) {}
         }
-
-        $msg = static::format($args[0],$args[2]);
+        if($format){
+            $msg = static::format($type,$log,$format);
+        }else{
+            $msg = date('Y-m-d H:i:s')."\t[{$type}]\t{$log}".PHP_EOL;
+        }
         return error_log($msg, 3, $destination);
     }
 
     /**
      * 格式化日志内容
-     * @param $content
-     * @param $format
+     * @param  $type
+     * @param  $content
+     * @param  $format
      * @return mixed
      */
-    static protected function format($content,$format)
+    static protected function format($type,$content,$format)
     {
         preg_match_all('/%t\(([^%]+)\)/',$format,$m);
         if(isset($m[1])){
@@ -86,7 +107,7 @@ class LogHelper
                 $format = str_replace($m[0][$i],date($m[1][$i]),$format);
             }
         }
-        $msg = str_replace('%C',$content,$format);
+        $msg = str_replace(['%T','%C'],[$type,$content],$format);
         return $msg.PHP_EOL;
     }
 
@@ -96,10 +117,10 @@ class LogHelper
      * @param $name
      * @param $arguments
      */
-    static public function __callStatic($name, $arguments)
+    public function __call($name, $arguments)
     {
         if(in_array($name,self::$type)) {
-            array_push($arguments,$name);
+            array_unshift($arguments,$name);
         }
         call_user_func_array(self::class.'::record',$arguments);
     }
