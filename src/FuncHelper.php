a05a3c01d780141e956a7d5b61eb2e7521bbd8ff
liansen <liansen@d3zz.com>
2018-07-13 11:46:30 +0800

    增加电话号码去除无用字符函数

--- src/FuncHelper.php
+++ src/FuncHelper.php
@@ -59,5 +59,38 @@ final  class FuncHelper
         if($exit) exit;
     }
 
+    /**
+     * 返回指定长度的电话号码<br/>
+     * 原理是<br/>
+     * 1.去除$phone中的所有非数字字符<br/>
+     * 2.截取剩余字符中的后$length个字符
+     * @param string $phone    电话号码
+     * @param int $length      电话号码长度(默认中国大陆11位)
+     * @return string
+     */
+    static public function trim_phone($phone,$length = 11)
+    {
+        $phone = trim($phone);
+        $phone = preg_replace('/[^0-9]/','',$phone);
+        return substr($phone,0-$length);
+    }
+
+
+    /**
+     * 检查$phone经过trim_phone函数处理后是否是一个有效的大陆手机号码<br/>
+     * 如果是，返回经过处理后的普通电话号码<br/>
+     * 否则，返回false
+     * @param $phone
+     * @return string|bool
+     */
+    static public function phone($phone)
+    {
+        $phone = self::trim_phone($phone);
+        if(!!preg_match('/^1[3-9]\d{9}$/',$phone)){
+            return $phone;
+        }
+        return false;
+    }
+
 
 }
